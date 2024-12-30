788ad44ca7529da2d0804a7ab572716eba23adbe
Alejandro Zárate <u20181h198@upc.edu.pe>
2024-12-30 08:25:04 -0500

    Estructura Inicial - ESPACEA UC

---
+++ src/app/page.js
@@ -0,0 +1,81 @@
+import BannerOne from "@/component/banner/BannerOne";
+import HeaderOne from "@/component/header/HeaderOne";
+
+
+import FooterOne from "@/component/footer/FooterOne";
+import BackToTop from "@/component/backtotop/BackToTop";
+import AboutOne from "@/component/about/AboutOne";
+import ServiceOne from "@/component/service/ServiceOne";
+import ProcessOne from "@/component/workingprocess/ProcessOne";
+import ProjectOne from "@/component/project/ProjectOne";
+import ProjectTwo from "@/component/project/ProjectTwo";
+import CounterUpOne from "@/component/counterup/CounterUpOne";
+import TestimonialsOne from "@/component/testimonials/TestimonialsOne";
+import BlogOne from "@/component/blog/BlogOne";
+import TeamOne from "@/component/team/TeamOne";
+import BrandOne from "@/component/brand/BrandOne";
+import HeaderTwo from "@/component/header/HeaderTwo";
+import BannerTwo from "@/component/banner/BannerTwo";
+import AboutTwo from "@/component/about/AboutTwo";
+import ServiceTwo from "@/component/service/ServiceTwo";
+import WhyChoose from "@/component/whychoose/WhyChoose";
+import VideoOne from "@/component/video/VideoOne";
+import CounterUpTwo from "@/component/counterup/CounterUpTwo";
+import TestimonialsTwo from "@/component/testimonials/TestimonialsTwo";
+import BrandTwo from "@/component/brand/BrandTwo";
+import CtaOne from "@/component/cta/CtaOne";
+import FooterTwo from "@/component/footer/FooterTwo";
+
+
+
+export default function Home() {
+  return (
+    <div className="#">
+
+            <HeaderTwo />
+            <BannerTwo />
+            <ServiceTwo />
+            <AboutTwo />
+            <WhyChoose />
+            <VideoOne />
+            <ProjectTwo />
+            <CounterUpTwo />
+            <TestimonialsTwo />
+            <BrandTwo />
+            <BlogOne />
+            <CtaOne />
+            <FooterTwo />
+
+
+
+
+            <BackToTop />
+    </div>
+  );
+}
+
+
+/*
+export default function Home() {
+  return (
+    <div className="#">
+
+      <HeaderOne />
+      <BannerOne />
+      <AboutOne />
+      <ServiceOne />
+      <ProcessOne />
+      <ProjectOne />
+      <CounterUpOne />
+      <BrandOne />
+      <TeamOne />
+      <TestimonialsOne />
+      <BlogOne />
+
+
+      <FooterOne />
+      <BackToTop />
+    </div>
+  );
+}
+*/
